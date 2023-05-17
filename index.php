16de02c5696ff758ea4e24411722515117de1949
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-05-17 14:34:30 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php 
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous'/>
+
+  <title>password generator</title>
+</head>
+<body>
+
+  <div class="container">
+    <h1>generatore di password super sicuro</h1>
+
+    <form action="index.php" method="GET">
+
+      <div class="row">
+        <div class="col">
+
+      </div>
+
+        <div class="col">
+
+        </div>
+      </div>
+
+    </form>
+
+  </div>
+  
+
+  
+</body>
+</html>
